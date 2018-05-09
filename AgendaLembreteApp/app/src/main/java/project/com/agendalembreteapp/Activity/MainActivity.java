690d5efe10c0c4e5e0d922baeef83be6f622ca97
GabrielPetz <petzgabriel@outlook.com>
2018-05-09 17:03:59 +0000

    AgendaLembreteApp

---
+++ AgendaLembreteApp/app/src/main/java/project/com/agendalembreteapp/Activity/MainActivity.java
@@ -0,0 +1,15 @@
+package project.com.agendaCompromissoapp.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import project.com.agendaCompromissoapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
